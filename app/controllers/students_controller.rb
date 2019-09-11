c2f8acf21ed116e8883d7142d403270fc56962ee
Solomon Feldman <solomonrfeldman@gmail.com>
2019-09-11 02:23:08 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,7 +21,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.search(params[:query])
+    @students = Student.(params[:query])
   end
 
   def student_params
